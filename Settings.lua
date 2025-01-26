d9bdfb63a9c5802226d9c67dbbc24969fe62cbed
VeNoM-1987 <Casperplooij1987@gmail.com>
2025-01-26 11:20:33 +0100

    Create Settings.lua

---
+++ Settings.lua
@@ -0,0 +1,16 @@
+getgenv().Commands =  {
+    RenderingEnabled = true
+}
+
+getgenv().settings = {
+    {
+        item = "Lightning Cannon",
+        maxPrice = 1000000,
+        class = "Misc"
+    }--[[,
+    {
+        item = "Nuclear Cannon",
+        maxPrice = 100000,
+        class = "Misc"
+    }]]
+}
