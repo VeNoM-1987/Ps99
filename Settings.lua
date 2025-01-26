84b42694d1551d8fe411d8dcea8de44aae3c7370
VeNoM-1987 <Casperplooij1987@gmail.com>
2025-01-26 18:07:00 +0100

    Update Settings.lua

--- Settings.lua
+++ Settings.lua
@@ -5,10 +5,15 @@ getgenv().Commands =  {
 
 getgenv().settings = {
     {
+        item = "Void Key Upper Half",
+        maxPrice = 7000,
+        class = "Misc"
+    }
+     --[[{
         item = "Lightning Cannon",
         maxPrice = 2000000,
         class = "Misc"
-    }--[[,
+    },
     {
         item = "Nuclear Cannon",
         maxPrice = 100000,
